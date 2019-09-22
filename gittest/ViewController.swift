fdd3ccf34fae3f2397fb95f79eee373b2d6aa0f3
Chigo <godwinanyaso@gmail.com>
2019-09-22 20:02:35 +0100

    Initial Commit

---
+++ gittest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gittest
+//
+//  Created by godwin anyaso on 22/09/2019.
+//  Copyright © 2019 godwin anyaso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
